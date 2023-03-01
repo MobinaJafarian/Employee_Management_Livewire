c1aed12c29057c5f7b73bc624f1131af1aa93872
Mobina <nimobina99@gmail.com>
2023-03-01 14:42:54 +0330

    user-index blade

---
+++ resources/views/livewire/users/user-index.blade.php
@@ -0,0 +1,186 @@
+<div>
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Users</h1>
+    </div>
+    <div class="row">
+        <div class="card  mx-auto">
+            <div>
+                @if (session()->has('user-message'))
+                    <div class="alert alert-success">
+                        {{ session('user-message') }}
+                    </div>
+                @endif
+            </div>
+            <div class="card-header">
+                <div class="row">
+                    <div class="col">
+                        <form>
+                            <div class="form-row align-items-center">
+                                <div class="col">
+                                    <input type="search" wire:model.defer="search" class="form-control mb-2"
+                                        id="inlineFormInput" placeholder="search">
+                                </div>
+                                <div class="col" wire:loading>
+                                    <div class="spinner-border" role="status">
+                                        <span class="sr-only">Loading...</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <button wire:click="showUserModal" class="btn btn-primary">
+                            New User
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <table class="table" wire:loading.remove>
+                    <thead>
+                        <tr>
+                            <th scope="col">#Id</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Manage</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($users as $user)
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->username }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <button wire:click="showEditModal({{ $user->id }})"
+                                        class="btn btn-success">Edit</button>
+                                    <button wire:click="deleteUser({{ $user->id }})"
+                                        class="btn btn-danger">Delete</button>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <th>No Results</th>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                {{ $users->links('pagination::bootstrap-4') }}
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="userModal" tabindex="-1" aria-labelledby="userModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="userModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group row">
+                                <label for="username"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="username" type="text"
+                                        class="form-control @error('name') is-invalid @enderror"
+                                        wire:model.defer="username">
+
+                                    @error('username')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="firstName"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('First Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="firstName" type="text"
+                                        class="form-control @error('name') is-invalid @enderror"
+                                        wire:model.defer="firstName">
+
+                                    @error('firstName')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="lastName"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="lastName" type="text"
+                                        class="form-control @error('name') is-invalid @enderror"
+                                        wire:model.defer="lastName">
+
+                                    @error('lastName')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="email"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror"
+                                        wire:model.defer="email">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            {{-- @if (!$editMode)
+                                <div class="form-group row">
+                                    <label for="password"
+                                        class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password"
+                                            class="form-control @error('password') is-invalid @enderror"
+                                            wire:model.defer="password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            @endif --}}
+
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" wire:click="closeModal">Close</button>
+                        {{-- @if ($editMode)
+                            <button type="button" class="btn btn-primary" wire:click="updateUser">Update User</button>
+                        @else
+                            <button type="button" class="btn btn-primary" wire:click="storeUser">Store User</button>
+                        @endif --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
