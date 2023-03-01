84308d469dcc4453ee8d5b89da46b01d9b01d513
Mobina <nimobina99@gmail.com>
2023-03-01 14:40:31 +0330

    add user-index route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Users\UserIndex;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('/users', UserIndex::class)->name('users.index');
+    // Route::get('/countries', CountryIndex::class)->name('countries.index');
+    // Route::get('/states', StateIndex::class)->name('states.index');
+    // Route::get('/cities', CityIndex::class)->name('cities.index');
+    // Route::get('/departments', DepartmentIndex::class)->name('departments.index');
+    // Route::get('/employees', EmployeeIndex::class)->name('employees.index');
 });
