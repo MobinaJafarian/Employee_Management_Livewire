4f668c7d3966e52471f2d0d75eec4aeecd8d82c9
Mobina <nimobina99@gmail.com>
2023-03-02 23:47:52 +0330

    add function store user

--- app/Http/Livewire/Users/UserIndex.php
+++ app/Http/Livewire/Users/UserIndex.php
@@ -3,11 +3,49 @@
 namespace App\Http\Livewire\Users;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class UserIndex extends Component
 {
     public $search = '';
+    public $username;
+    public $firstName;
+    public $lastName;
+    public $email;
+    public $password;
+    public $userId;
+    public $editMode =false;
+    protected $rules = [
+        'username' => 'required',
+        'firstName' => 'required',
+        'lastName' => 'required',
+        'password' => 'required',
+        'email' => 'required|email',
+    ];
+
+
+    public function storeUser()
+    {
+        $this->validate();
+
+        User::create([
+           'username' =>  $this->username,
+           'first_name' =>  $this->firstName,
+           'last_name' =>  $this->lastName,
+           'email' =>  $this->email,
+           'password' =>  Hash::make($this->password),
+       ]);
+        $this->reset();
+        $this->dispatchBrowserEvent('modal', ['modalId' => '#userModal', 'actionModal' => 'hide']);
+        session()->flash('user-message', 'User successfully created');
+    }
+
+    public function showUserModal()
+    {
+        $this->reset();
+        $this->dispatchBrowserEvent('modal', ['modalId' => '#userModal', 'actionModal' => 'show']);
+    }
 
     public function render()
     {
@@ -20,4 +58,6 @@ class UserIndex extends Component
         ])
                  ->layout('layouts.main');
     }
+
+
 }
