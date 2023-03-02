490c753afc44595e46592fed73637e30613985a4
Mobina <nimobina99@gmail.com>
2023-03-03 01:03:49 +0330

    add CountryRoute

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Country\CountryIndex;
 use App\Http\Livewire\Users\UserIndex;
 use Illuminate\Support\Facades\Route;
 
@@ -22,7 +23,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
     Route::view('/dashboard', 'dashboard')->name('dashboard');
     Route::get('/users', UserIndex::class)->name('users.index');
-    // Route::get('/countries', CountryIndex::class)->name('countries.index');
+    Route::get('/countries', CountryIndex::class)->name('countries.index');
     // Route::get('/states', StateIndex::class)->name('states.index');
     // Route::get('/cities', CityIndex::class)->name('cities.index');
     // Route::get('/departments', DepartmentIndex::class)->name('departments.index');
