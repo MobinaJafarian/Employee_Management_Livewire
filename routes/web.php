cf4b3f766cb47995b42478444d81c16666d2df65
Mobina <nimobina99@gmail.com>
2023-03-04 09:18:20 +0330

    add city route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\City\CityIndex;
 use App\Http\Livewire\Country\CountryIndex;
 use App\Http\Livewire\State\StateIndex;
 use App\Http\Livewire\Users\UserIndex;
@@ -26,7 +27,7 @@ Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])-
     Route::get('/users', UserIndex::class)->name('users.index');
     Route::get('/countries', CountryIndex::class)->name('countries.index');
     Route::get('/states', StateIndex::class)->name('states.index');
-    // Route::get('/cities', CityIndex::class)->name('cities.index');
+    Route::get('/cities', CityIndex::class)->name('cities.index');
     // Route::get('/departments', DepartmentIndex::class)->name('departments.index');
     // Route::get('/employees', EmployeeIndex::class)->name('employees.index');
 });
