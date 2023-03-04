3317dfc2ea42ac9126cf2744e5051f9d6c0e992f
Mobina <nimobina99@gmail.com>
2023-03-04 10:19:53 +0330

    update relation

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -11,8 +11,8 @@ class Department extends Model
     
     protected $fillable = ['name'];
 
-    // public function employees()
-    // {
-    //     return $this->hasMany(Employee::class);
-    // }
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
