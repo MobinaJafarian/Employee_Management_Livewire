98f0cc0d5a9f14f07a4e27ce6858b02558ade9ad
Mobina <nimobina99@gmail.com>
2023-03-05 11:58:06 +0330

    update footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -144,7 +144,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; Employee Management 2023</span>
                     </div>
                 </div>
             </footer>
